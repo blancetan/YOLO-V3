2a0ea0f800ba1e0ba75c6b8ffafaffa18b27b9ed
18145288 <2275501257@qq.com>
2020-02-18 15:51:23 +0800

    add data module

---
+++ data.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+#_*_ coding: utf-8 _*_
+
+"""
+ @DateTime: 18/2/2020 15:47
+ @Author:   balanceTan 
+ @File:     data.py
+ @Software: PyCharm
+ 
+"""
+
+data = [1, 2, 3, 4,]
+print(data)
